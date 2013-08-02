0e0dd9a2ffcff469c366a8b52e6a7d48661802c9
zhoujie <jie_zhou@allyes.com>
2013-08-02 14:20:05 +0800

    modify code

--- script/getObservReport.sh
+++ script/getObservReport.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+
 if [ $# -le 0 ]
 then
   echo 'you must pass the necessary parameter using "--hdfsHostUrl hdfs://sm98:9000  --observReportHDFSDir /user/hadoop/mifc_etl/output_observer_report  --observReportBEDir  /home/hadoop/zhoujie/project/hdfsutils"'
   exit;
 fi
 
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+echo "current dir:$bin"
+
+#exit
 while [ -n "$1" ]; do
   case "$1" in
     --hdfsHostUrl)
@@ -34,5 +40,4 @@ echo "$observReportHDFSDir"
 echo "$observReportBEDir"
 #exit
 
-#java -cp ObservReportUtil/hdfsutils.jar com.allyes.hdfsutils.GetObservReportJSON --hdfsHostUrl=hdfs://sm98:9000  --observReportHDFSDir=/user/hadoop/mifc_etl/output_observer_report  --observReportBEDir=/home/hadoop/zhoujie/project/hdfsutils
-java -cp ObservReportUtil/hdfsutils.jar com.allyes.hdfsutils.GetObservReportJSON --hdfsHostUrl=${hdfsHostUrl} --observReportHDFSDir=${observReportHDFSDir} --observReportBEDir=${observReportBEDir}
+java -cp ${bin}/ObservReportUtil/hdfsutils.jar com.allyes.hdfsutils.GetObservReportJSON --hdfsHostUrl=${hdfsHostUrl} --observReportHDFSDir=${observReportHDFSDir} --observReportBEDir=${observReportBEDir}
