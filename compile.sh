3b04ce5229e5edc54e2c841d66aabc8b3d392d95
zhoujie <jie_zhou@allyes.com>
2013-08-02 11:29:50 +0800

    add info

--- compile.sh
+++ compile.sh
@@ -5,3 +5,11 @@ git rebase origin/master
 
 ant clean
 ant jar
+
+echo "copy jar "
+cp build/*.jar script/ObservReportUtil
+
+echo "copy lib"
+cp -r lib  script/ObservReportUtil
+
+
---
+++ script/getObservReport.sh
@@ -0,0 +1,1 @@
+java -cp ObservReportUtil/hdfsutils.jar com.allyes.hdfsutils.GetObservReportJSON --hdfsHostUrl=hdfs://sm98:9000  --observReportHDFSDir=/user/hadoop/mifc_etl/output_observer_report  --observReportBEDir=/home/hadoop/zhoujie/project/hdfsutils
